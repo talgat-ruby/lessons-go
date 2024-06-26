f223f5258a7629165c96904635abbe08e0934d1a
Talgat Saribayev <talgat.s@protonmail.com>
2024-06-26 16:02:08 +0500

    add lesson10

---
+++ lesson10/example1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var x = 0
+
+func increment(wg *sync.WaitGroup, m *sync.Mutex) {
+	defer wg.Done()
+	m.Lock()
+	x = x + 1
+	m.Unlock()
+}
+
+func main() {
+	var w sync.WaitGroup
+	var m sync.Mutex
+	for i := 0; i < 1000; i++ {
+		w.Add(1)
+		go increment(&w, &m)
+	}
+	w.Wait()
+	fmt.Println("final value of x", x)
+}
