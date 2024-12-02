febc0e85c7a9dfa53b1bdf2125709869a6f34fc6
Talgat <talgat.s@protonmail.com>
2024-12-02 20:29:56 +0500

    feat(projects): fix the problem with no id found

--- projects/movie-reservation/internal/db/movie/delete_movie.go
+++ projects/movie-reservation/internal/db/movie/delete_movie.go
@@ -2,6 +2,7 @@ package movie
 
 import (
 	"context"
+	"fmt"
 )
 
 func (m *Movie) DeleteMovie(ctx context.Context, id int64) error {
@@ -12,10 +13,21 @@ DELETE FROM movie
 WHERE id = $1
 `
 
-	_, err := m.db.ExecContext(ctx, stmt, id)
+	res, err := m.db.ExecContext(ctx, stmt, id)
 	if err != nil {
 		log.ErrorContext(ctx, "fail to delete from the table movie", "error", err)
 		return err
+	}
+
+	num, err := res.RowsAffected()
+	if err != nil {
+		log.ErrorContext(ctx, "fail to delete from the table movie", "error", err)
+		return err
+	}
+
+	if num == 0 {
+		log.WarnContext(ctx, "movie with id was not found", "id", id)
+		return fmt.Errorf("movie with id was not found")
 	}
 
 	log.InfoContext(ctx, "success delete from the table movie")
--- projects/movie-reservation/internal/db/movie/update_movie.go
+++ projects/movie-reservation/internal/db/movie/update_movie.go
@@ -2,6 +2,7 @@ package movie
 
 import (
 	"context"
+	"fmt"
 )
 
 func (m *Movie) UpdateMovie(ctx context.Context, id int64, insertData *ModelMovie) error {
@@ -13,10 +14,21 @@ SET title = $2, description = $3, posterUrl = $4
 WHERE id = $1
 `
 
-	_, err := m.db.ExecContext(ctx, stmt, id, insertData.Title, insertData.Description, insertData.PosterURL)
+	res, err := m.db.ExecContext(ctx, stmt, id, insertData.Title, insertData.Description, insertData.PosterURL)
 	if err != nil {
 		log.ErrorContext(ctx, "fail to update the table movie", "error", err)
 		return err
+	}
+
+	num, err := res.RowsAffected()
+	if err != nil {
+		log.ErrorContext(ctx, "fail to update from the table movie", "error", err)
+		return err
+	}
+
+	if num == 0 {
+		log.WarnContext(ctx, "movie with id was not found", "id", id)
+		return fmt.Errorf("movie with id was not found")
 	}
 
 	log.InfoContext(ctx, "success update the table movie")
