fa4f8b94be97288c841763c7b1ba23cc19ada20d
Talgat <talgat.s@protonmail.com>
2024-12-05 18:32:53 +0500

    feat(projects): fix gracefull shutdown

--- projects/movie-reservation/main.go
+++ projects/movie-reservation/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
 
 	"github.com/joho/godotenv"
 
@@ -13,7 +14,7 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 
 	_ = godotenv.Load()
 
@@ -28,7 +29,25 @@ func main() {
 	}
 
 	a := api.New(slog.With("service", "api"), d)
-	if err := a.Start(ctx); err != nil {
-		panic(err)
-	}
+	go func(ctx context.Context, cancelFunc context.CancelFunc) {
+		if err := a.Start(ctx); err != nil {
+			slog.ErrorContext(ctx, "failed to start api", "error", err.Error())
+		}
+
+		cancelFunc()
+	}(ctx, cancel)
+
+	go func(cancelFunc context.CancelFunc) {
+		shutdown := make(chan os.Signal, 1)   // Create channel to signify s signal being sent
+		signal.Notify(shutdown, os.Interrupt) // When an interrupt is sent, notify the channel
+
+		sig := <-shutdown
+		slog.WarnContext(ctx, "signal received - shutting down...", "signal", sig)
+
+		cancelFunc()
+	}(cancel)
+
+	<-ctx.Done()
+
+	fmt.Println("shutting down gracefully")
 }
